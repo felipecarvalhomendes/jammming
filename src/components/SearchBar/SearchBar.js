fffc2d831494c51ef55c7e116a6ffeeb4c53e232
Felipe Mendes <felipecarvalhomendes@gmail.com>
2024-05-02 11:05:28 -0300

    Draft SearchBar and SearchResults components

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,7 +4,10 @@ import styles from './SearchBar.module.css'
 function SearchBar() {
     return (
         <div>
-
+            <form>
+                <input type='text' />
+                <button type='submit'>Search</button>
+            </form>
         </div>
     );
 }
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -4,9 +4,41 @@ import styles from './SearchResults.module.css'
 function SearchResults() {
     return (
         <div>
-
+            <h2>Results</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Song name 1</td>
+                        <td>Artist 1</td>
+                        <td>
+                            <button>Add</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3"><hr /></td> 
+                    </tr>
+                    <tr>
+                        <td>Song name 2</td>
+                        <td>Artist 2</td>
+                        <td>
+                            <button>Add</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3"><hr /></td> 
+                    </tr>
+                    <tr>
+                        <td>Song name 3</td>
+                        <td>Artist 3</td>
+                        <td>
+                            <button>Add</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
+
 
 export default SearchResults;
