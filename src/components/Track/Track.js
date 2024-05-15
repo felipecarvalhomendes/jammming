1c31089ad175903cf14b23fff2c0a4d3b9405c9b
Felipe Mendes <felipecarvalhomendes@gmail.com>
2024-05-15 13:24:45 -0300

    Start writing sample play

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,7 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import styles from './Track.module.css';
 
 function Track(props) {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const audio = new Audio(props.track.preview_url);
+
     const addTrack = useCallback(() => {
         props.onAdd(props.track);
     }, [props.onAdd, props.track]);
@@ -9,6 +12,15 @@ function Track(props) {
     const removeTrack = useCallback(() => {
         props.onRemove(props.track);
     }, [props.onRemove, props.track]);
+
+    const toggleSample = useCallback(() => {
+        if (isPlaying) {
+            audio.pause();
+        } else {
+            audio.play();
+        }
+        setIsPlaying(!isPlaying);
+    }, [isPlaying, audio]);
 
     const renderAction = () => {
         if (props.isRemoval) {
@@ -32,6 +44,11 @@ function Track(props) {
             <div className={styles.trackInfo}>
                 <h3>{props.track.name}</h3>
                 <h4>{props.track.artist} | {props.track.album}</h4>
+                {props.track.preview_url && (
+                    <button onClick={toggleSample}>
+                        {isPlaying ? 'Pause' : 'Play'}
+                    </button>
+                )}
             </div>
             {renderAction()}
 
